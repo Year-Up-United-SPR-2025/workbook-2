39a338afc184a0f28eac6d6ad4e2a9aa865fafa1
Jordan Alexis <42786923+Jordan721@users.noreply.github.com>
2025-04-21 09:23:05 -0400

    did Ex 1 LoopingEx and ForLoop Ex

---
+++ LoopingExercises/src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        String ILJ = "I love Java";
+    int i = 0;
+    do {
+        System.out.println(ILJ);
+        i++;
+    } while (i <=5);
+
+    }
+}
